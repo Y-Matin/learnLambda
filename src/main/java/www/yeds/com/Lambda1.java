e7b329f16c1b7bf3e9112b43fc31b0aab57d887f
Y-Martin <724797522@qq.com>
2019-09-16 20:44:12 +0800

    init

---
+++ src/main/java/www/yeds/com/Lambda1.java
@@ -0,0 +1,17 @@
+package www.yeds.com;
+
+/**
+ * @ProjectName: learnLambda
+ * @Package: www.yeds.com
+ * @ClassName: Lambda1
+ * @Description: TODO
+ * @author: yeds
+ * @date: 2019/9/16 20:36
+ * @version: V1.0
+ */
+public class Lambda1 {
+
+    public static void main(String[] args) {
+        Runnable run1 = () -> {};
+    }
+}
