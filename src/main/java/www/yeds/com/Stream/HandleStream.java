80c3d5732020535bb45ff283c8d9c2c280c5d503
Y-Martin <724797522@qq.com>
2019-11-14 22:00:03 +0800

    add some functions

--- src/main/java/www/yeds/com/Stream/HandleStream.java
+++ src/main/java/www/yeds/com/Stream/HandleStream.java
@@ -1,6 +1,8 @@
 package www.yeds.com.Stream;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @ProjectName: learnLambda
@@ -15,6 +17,7 @@ public class HandleStream {
     public static void main(String[] args) {
         List<String> stringList = Arrays.asList("1", "123", "1234", "12345","123456");
         List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> mixedList = Arrays.asList(1, 12, 345, 2, 8, 0, 3, 6, 56);
         // 过滤操作(过滤掉字符长度小于2的)
         stringList.stream().filter(x -> x.length() > 2).forEach(System.out::println);
         System.out.println("-------");
@@ -28,11 +31,45 @@ public class HandleStream {
         int sum = integerList.stream().filter(x -> x % 2 == 0).mapToInt(x -> x).sum();
         System.out.println("sum:" + sum);
         System.out.println("-------");
-        OptionalInt max = integerList.stream().filter(x -> x % 2 == 0).mapToInt(x -> x).max();
-        System.out.println("max:" + max);
+        //过滤（奇数）、取最大值
+        int asInt = integerList.stream().filter(x -> x % 2 == 0).mapToInt(x -> x).max().getAsInt();
+        System.out.println("max:" + asInt);
+        // 取最大值，使用Comparator
+        Integer max = integerList.stream().max(Comparator.comparingInt(o -> o)).get();
+        System.out.println("max1:"+max);
+        System.out.println("-------");
+        // 查找、findAny/findFirst
+        Optional<Integer> any = integerList.stream().findAny();
+        System.out.println(any.get());
+        System.out.println("-------");
+        //排序 sored ，正反序
+        mixedList.stream().sorted().forEach(System.out::println);
+        System.out.println("-------");
+        Integer firstMix = mixedList.stream().sorted().findFirst().get();
+        System.out.println("sored-up: first： "+firstMix);
+        System.out.println("-------");
+        mixedList.stream().sorted((o1, o2) -> o2-o1).forEach(System.out::println);
+        System.out.println("-------");
+        Integer firstMax = mixedList.stream().sorted((o1, o2) -> o2-o1).findFirst().get();
+        System.out.println("sored-down: first："+firstMax);
+        System.out.println("-------");
+        //截取，limite、skip
+        Stream.iterate(1, x -> x + 1).limit(50).forEach(System.out::println);
+        System.out.println("-------");
+        Stream.iterate(1, x -> x + 1).limit(50).filter(x->x%2==0).forEach(System.out::println);
+        System.out.println("-------");
+        Stream.iterate(1, x -> x + 1).limit(50).filter(x -> x % 2 == 0).skip(10).forEach(System.out::println);
+        System.out.println("-------");
+        // 收集器 collect()
+        List<Integer> collect = Stream.iterate(1, x -> x + 1).limit(10).collect(Collectors.toList());
+        System.out.println(collect);
+        // collect() ,toSet() 去重
+        Set<Integer> set = Arrays.asList(1, 2, 3, 3, 5, 1).stream().collect(Collectors.toSet());
+        System.out.println(set);
+        // 去重 distinct()
+        List<Integer> collect1 = Arrays.asList(1, 2, 3, 3, 5, 1).stream().distinct().collect(Collectors.toList());
+        System.out.println(collect1);
 
-        Optional<Integer> max1 = integerList.stream().max(Comparator.comparingInt(a -> a));
-        System.out.println("max1:"+max1);
 
     }
 }
